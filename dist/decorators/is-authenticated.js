deff7028abe3233b5a37488e947d83d7c04e8260
Harry Brundage <harry.brundage@gmail.com>
2021-02-22 18:31:41 -0500

    gitpkg

---
+++ dist/decorators/is-authenticated.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.isAuthenticated = void 0;
+function isAuthenticated() {
+    let property = 'user';
+    if (this.passport && this.passport) {
+        property = this.passport.userProperty || 'user';
+    }
+    return this[property] ? true : false;
+}
+exports.isAuthenticated = isAuthenticated;
